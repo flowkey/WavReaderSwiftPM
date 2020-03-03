3c4c53f6ec878469f19237543288277617815b9f
Geordie J <geojay@gmail.com>
2020-03-03 16:22:33 +0100

    Initial Commit

---
+++ readWav/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  readWav
+//
+//  Created by Geordie Jay on 03.03.20.
+//  Copyright © 2020 flowkey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
